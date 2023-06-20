062da5f58af382eceb2dd8701ef25f9cd63928c5
Celinaki <celina1999@live.se>
2023-06-20 23:33:21 +0200

    some responsiveness(btn content) added to navbar, mobilefirst

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,36 +1,47 @@
 import type { GatsbyConfig } from "gatsby";
 
-require("dotenv").config()
-
-require("dotenv").config({
-
-  path: `.env.${process.env.NODE_ENV}`,
-
-})
+require("dotenv").config();
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `celinasportfolio`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken":  process.env.CONTENTFUL_ACCESS_TOKEN,
-      "spaceId": process.env.CONTENTFUL_SPACE_ID
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID
+      }
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: "images",
+        path: "./src/images/"
+      },
+      __key: "images"
+    },
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: [
+          "Monospace Font:400,700",
+          `Montserrat`
+           // Replace with the desired font name and variants
+        ],
+    
+        display: "swap" // Set to 'swap' for better performance
+      }
+    }
+  ]
 };
 
 export default config;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import * as styles from '../styles/navbar.module.scss';
+import {Link} from "gatsby";
+import type { HeadFC, PageProps } from "gatsby";
+import Menubutton from '../images/menu_FILL0_wght400_GRAD0_opsz48.svg';
+import Closebutton from '../images/close_FILL0_wght400_GRAD0_opsz48.svg'
 
 const Navbar = () => {
+const [menuActive, setMenuActive ] = useState<Boolean>(false)
+
+useEffect(()=>{
+
+},[menuActive])
   return (
-    <div className={styles.header}>
-      <h1>hej</h1>
-    </div>
+    <nav className={styles.navwrapper}>
+        <section className={styles.logo}>
+            <a href="#">CS</a>
+        </section>
+      <section className={` ${menuActive ? styles.activenavlist : styles.navlist}`}>
+        <Link to="/" >Home</Link>
+        <Link to="/" >Projects </Link>
+        <Link to="/" >About </Link>
+        <Link to="/" >Contact</Link>
+      </section>
+      <div className={styles.togglebutton}> 
+      {
+        menuActive ?
+         <img 
+         className={styles.closeicon} 
+         src={Closebutton} alt="" 
+         onClick={()=>setMenuActive(false)}
+         />
+         :
+                <img 
+         className={styles.menuicon}
+         src={Menubutton} alt=""
+         onClick={()=>setMenuActive(true)} />
+      }
+  
+        
+       </div>
+    </nav>
   );
 };
+export const Head: HeadFC = () => <head><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+</head>
 
 export default Navbar;
