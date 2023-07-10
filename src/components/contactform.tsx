1cddc62e838ace643335d81e613a4c2db24b32b2
Celinaki <celina1999@live.se>
2023-07-10 19:49:12 +0200

    validation done with controls of each field

--- src/components/contactform.tsx
+++ src/components/contactform.tsx
@@ -5,12 +5,21 @@ import * as style from "../styles/contactform.module.scss"
 
 export const Contactform = () => {
 const [emailSent, setEmailSent] = useState<Boolean>(false)
+const [name, setName] = useState<string>('');
+const [email, setEmail] = useState<string>('');
+const [message, setMessage] = useState<string>('');
+const [nameError, setNameError] = useState<Boolean>(false)
+const [emailError, setEmailError] = useState<Boolean>(false)
+const [messageError, setMessageError] = useState<Boolean>(false)
+const [allValid, setAllValid] = useState<Boolean>(false)
+
 
     const form = useRef<HTMLFormElement>(null);
 
     const sendEmail = (e: React.FormEvent) => {
-      e.preventDefault();
-        emailjs
+      e.preventDefault();    
+      if(allValid){
+          emailjs
         .sendForm(
             process.env.GATSBY_YOUR_SERVICE_ID! as string,
             process.env.GATSBY_YOUR_TEMPLATE_ID! as string,
@@ -21,28 +30,124 @@ const [emailSent, setEmailSent] = useState<Boolean>(false)
             (result) =>{
                 console.log(result.text);
                 setEmailSent(true)
+                setName('');
+                setEmail('');
+                setMessage('');
             },
             (error) =>{
                 console.log(error.text)
             }
-        );
-    };
-
+        ); 
+      }
       
+    };
+    useEffect(()=>{
+      sendEmail
+    },[])
+
+
+
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setName(event.target.value);
+    };
+  
+    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(event.target.value);
+    };
+  
+    const handleMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setMessage(event.target.value);
+    };
+      useEffect(()=>{
+    handleNameChange;
+    handleEmailChange;
+    handleMessageChange;
+ 
+  },)
+
+  useEffect(()=>{
+       if(message.length> 3){
+      setMessageError(false)
+    }  if(email.length > 3 && email.includes("@") && email.includes(".")){
+      setEmailError(false)}
+      if(name.length > 1){
+        setNameError(false)
+      }
+  },[message, email, name])
+  useEffect(()=>{
+    if(message.length> 3 && email.length > 3 && email.includes("@") && email.includes(".") && name.length > 2){
+      setAllValid(true)
+    }
+  })
+  
+  const checkName =(event: React.FocusEvent<HTMLInputElement>)=>{
+    if(name.length > 2){
+      setNameError(false)
+    }else setNameError(true)
+    }
+  const checkEmail =(event: React.FocusEvent<HTMLInputElement>)=>{
+    if(email.length > 3 && email.includes("@") && email.includes(".")){
+      setEmailError(false)
+    }else setEmailError(true)
+    }
+
+  const checkMessage =(event: React.FocusEvent<HTMLTextAreaElement>)=>{
+  if(message.length > 3){
+    setMessageError(false)
+  }else setMessageError(true)
+  }
+
   return (
-    <div>
+    <div>    
+
      {emailSent  ? 
-    <Snackbar message={"Email is now sent"}/>
+     <Snackbar message={"Email is now sent"}/>
     :
     '' 
     } 
        <form ref={form} onSubmit={sendEmail} className={style.form}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      {
+        nameError ?
+        <label  className={style.errormessage}>* Please enter your name</label>
+        :
+        ''
+
+      }
+
+      <input type="text"
+       name="user_name"
+       value={name}
+       onChange={handleNameChange}
+       onBlur={checkName}/>
       <label>Email</label>
-      <input type="email" name="user_email" />
+      {
+        emailError ?
+        <label  className={style.errormessage}>* Please enter a valid email</label>
+        :
+        ''
+
+      }
+      <input type="email" 
+      name="user_email"  
+      value={email}
+
+      onChange={handleEmailChange}
+      onBlur={checkEmail}
+      />
       <label>Message</label>
-      <textarea name="message" className={style.messagetext} />
+      {
+        messageError ?
+        <label  className={style.errormessage}>* Type your message </label>
+        :
+        ''
+      }
+      <textarea name="message" 
+             value={message}
+
+      className={style.messagetext}  
+      onChange={handleMessageChange} 
+      onBlur={checkMessage}/>
       <button type="submit" value="Send" > Send</button>
     </form>
     </div>
