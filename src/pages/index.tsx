2bd4c9a9508f199f626703d1ec7043ac0b52b6a6
Celinaki <celina1999@live.se>
2023-07-15 01:10:22 +0200

    playing around with a video-hero for desktop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,13 @@ import About from "../components/about"
 import Contact from "../components/contact"
 import Projects from "../components/projects"
 import Contactform from "../components/contactform"
+import { useMediaQuery } from 'react-responsive';
+import Video from '../images/IMG_1514.mp4'
+
 
 const IndexPage: React.FC<PageProps> = () => {
+  const isDesktop = useMediaQuery({ minWidth: 769 });
+
   return (
     <main>
       <Navbar></Navbar>
@@ -15,6 +20,23 @@ const IndexPage: React.FC<PageProps> = () => {
 <section>
 
 </section>
+{
+  isDesktop 
+  ?
+  <div style={{height:"90vh", position:"relative"}}>
+
+    <div style={{position:"absolute", height:"80%", backgroundColor:"#d1cece",borderRadius:"20px", width:"40%", left:20, top:20, opacity:0.6, zIndex:2}}>
+
+    </div>
+   
+    <div style={{position:"absolute", height:"80%", backgroundColor:"#bbb3b3",borderRadius:"20px", width:"40%", left:60, top:60, opacity:0.4}}></div>
+
+  <video 
+  
+  src={Video} style={{width:"100%", height:"100%", objectFit:"cover", borderRadius:"20px"}} autoPlay loop ></video></div>
+  :
+  ''
+}
       <h1>Projects</h1>
       
       <Projects></Projects>
--- src/sass.d.ts
+++ src/sass.d.ts
@@ -2,3 +2,5 @@ declare module "*.module.scss";
 
 declare module '*.svg';
 declare module '*.png';
+declare module '*.mov';
+declare module '*.mp4';
