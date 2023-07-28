47989e7e202d010fb94838083e4f4e4374e081bb
Celinaki <celina1999@live.se>
2023-07-28 13:21:27 +0200

    created static query for projects, removed dummydata. replaced dummydata with real

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,92 +1,50 @@
 import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import type { HeadFC, PageProps, Link } from "gatsby"
+import { graphql, useStaticQuery } from 'gatsby';
 import * as style from "../styles/oneproject.module.scss"
 import GitIcon from "../images/Octicons-mark-github.svg"
 import Openexternal from "../images/external-link.svg"
-const Projects: React.FC<PageProps> = () => {
-    const examples = [
-        {
-            title: "Lindcode",
-            image: "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum doloribus maxime consectetur ea itaque recusandae, distinctio, temporibus voluptas ullam eaque sapiente iste explicabo eum! Cupiditate perferendis cum laboriosam minima accusamus. Doloremque, tempora facere ullam cumque earum repellendus a obcaecati quas molestiae tempore magni consequuntur. Accusantium facere magnam accusamus molestias.",
-            tags: ["React", "Fullstack", "Figma", "MongoDB"],
-            category: "React",
-            linkToThePage: "url",
-            linkToGithub: "url",
 
-        },
-        {
-            title: "Nilo collab",
-            image: "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum doloribus maxime consectetur ea itaque recusandae, distinctio, temporibus voluptas ullam eaque sapiente iste explicabo eum! Cupiditate perferendis cum laboriosam minima accusamus. Doloremque, tempora facere ullam cumque earum repellendus a obcaecati quas molestiae tempore magni consequuntur. Accusantium facere magnam accusamus molestias.",
-            tags: ["MUI", "React"],
-            category: "React",
-            linkToThePage: "url",
-            linkToGithub: "url",
+interface Props {
+    allContentfulProject: {
+    nodes: Array< {
+            title: string;
+            id: string;
+            image: Array<{
+            url: string;
+            }>;
+            hashtags: string[];  
+    }>       
+   }
+  }
 
-        },
-        {
-            title: "ACNH",
-            image: "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum doloribus maxime consectetur ea itaque recusandae, distinctio, temporibus voluptas ullam eaque sapiente iste explicabo eum! Cupiditate perferendis cum laboriosam minima accusamus. Doloremque, tempora facere ullam cumque earum repellendus a obcaecati quas molestiae tempore magni consequuntur. Accusantium facere magnam accusamus molestias.",
-            tags: ["React", "Fullstack", "Figma", "MongoDB"],
-            category: "React",
-            linkToThePage: "url",
-            linkToGithub: "url",
-
-        },
-        {
-            title: "Css cards",
-            image: "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum doloribus maxime consectetur ea itaque recusandae, distinctio, temporibus voluptas ullam eaque sapiente iste explicabo eum! Cupiditate perferendis cum laboriosam minima accusamus. Doloremque, tempora facere ullam cumque earum repellendus a obcaecati quas molestiae tempore magni consequuntur. Accusantium facere magnam accusamus molestias.",
-            tags: ["React", "Fullstack", "Figma", "MongoDB"],
-            category: "React",
-            linkToThePage: "url",
-            linkToGithub: "url",
-
-        },
-        {
-            title: "Company",
-            image: "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum doloribus maxime consectetur ea itaque recusandae, distinctio, temporibus voluptas ullam eaque sapiente iste explicabo eum! Cupiditate perferendis cum laboriosam minima accusamus. Doloremque, tempora facere ullam cumque earum repellendus a obcaecati quas molestiae tempore magni consequuntur. Accusantium facere magnam accusamus molestias.",
-            tags: ["React", "Fullstack", "Figma", "MongoDB"],
-            category: "React",
-            linkToThePage: "url",
-            linkToGithub: "url",
-
-        },
-        {
-            title: "Recipe site",
-            image: "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum doloribus maxime consectetur ea itaque recusandae, distinctio, temporibus voluptas ullam eaque sapiente iste explicabo eum! Cupiditate perferendis cum laboriosam minima accusamus. Doloremque, tempora facere ullam cumque earum repellendus a obcaecati quas molestiae tempore magni consequuntur. Accusantium facere magnam accusamus molestias.",
-            tags: ["React", "Fullstack", "Figma", "MongoDB"],
-            category: "React",
-            linkToThePage: "url",
-            linkToGithub: "url",
-
-        },
-        {
-            title: "Starwars",
-            image: "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum doloribus maxime consectetur ea itaque recusandae, distinctio, temporibus voluptas ullam eaque sapiente iste explicabo eum! Cupiditate perferendis cum laboriosam minima accusamus. Doloremque, tempora facere ullam cumque earum repellendus a obcaecati quas molestiae tempore magni consequuntur. Accusantium facere magnam accusamus molestias.",
-            tags: ["React", "Fullstack", "Figma", "MongoDB"],
-            category: "React",
-            linkToThePage: "url",
-            linkToGithub: "url",
-
+ const Projects: React.FC = ( ) => {
+    const data: Props = useStaticQuery(graphql`
+    query {
+    allContentfulProject {
+      nodes {
+        title
+        id
+        image  {
+          url
         }
-    ]
+       hashtags
+      } 
+  }
+}
+`
+  )
 
     return (
         <div  id="projects">
             <h1>Projects</h1>
-            <div>
-            {examples.map(project =>
-                <div className={style.projectwrapper}>
+            <div className={style.projectscontainer}>
+            {data.allContentfulProject.nodes.map(project =>
+                <div className={style.projectwrapper} key={project.id} >
                     <h2>{project.title}</h2>
-                    <img src={project.image} alt="" className={style.projectimg} />
+                    <img src={project.image[0].url} alt="" className={style.projectimg} />
                     <span>
-                        {project.tags.map(tag =>
+                        {project.hashtags.map(tag =>
                             <p>{tag}</p>
                         )}
                     </span>
@@ -106,3 +64,4 @@ const Projects: React.FC<PageProps> = () => {
     )
 }
 export default Projects;
+
