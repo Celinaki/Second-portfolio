34cfd270321decf0d8ef61c60bddacd1495c2576
Celinaki <celina1999@live.se>
2023-07-30 17:35:24 +0200

    imported links and placed in right place on each projectcard

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,24 +1,29 @@
 import * as React from "react"
-import type { HeadFC, PageProps, Link } from "gatsby"
-import { graphql, useStaticQuery } from 'gatsby';
+import type { HeadFC, PageProps } from "gatsby"
+import { graphql, useStaticQuery , Link} from 'gatsby';
 import * as style from "../styles/oneproject.module.scss"
 import GitIcon from "../images/Octicons-mark-github.svg"
 import Openexternal from "../images/external-link.svg"
+import { renderRichText } from "gatsby-source-contentful/rich-text";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 interface Props {
     allContentfulProject: {
-    nodes: Array< {
+        nodes: Array<{
             title: string;
             id: string;
             image: Array<{
-            url: string;
+                url: string;
             }>;
-            hashtags: string[];  
-    }>       
-   }
-  }
+            hashtags: string[];
+            githubLink?: string;
+            pageLink?: string;
+        }>;
 
- const Projects: React.FC = ( ) => {
+    }
+}
+
+const Projects: React.FC = () => {
     const data: Props = useStaticQuery(graphql`
     query {
     allContentfulProject {
@@ -28,38 +33,52 @@ interface Props {
         image  {
           url
         }
-       hashtags
+        hashtags
+        githubLink
+        pageLink
       } 
+
   }
 }
 `
-  )
+    )
+    console.log(data.allContentfulProject.nodes)
 
     return (
-        <div  id="projects">
+        <div id="projects" className={style.projects}>
             <h1>Projects</h1>
             <div className={style.projectscontainer}>
-            {data.allContentfulProject.nodes.map(project =>
-                <div className={style.projectwrapper} key={project.id} >
-                    <h2>{project.title}</h2>
-                    <img src={project.image[0].url} alt="" className={style.projectimg} />
-                    <span>
-                        {project.hashtags.map(tag =>
-                            <p>{tag}</p>
-                        )}
-                    </span>
-                    <section className={style.buttonscontainer}>
-                    <span className={style.linkscontainer}>  
-                        <img src={GitIcon} alt="" className={style.projecticons} />
-                        <img src={Openexternal} alt="" className={style.projecticons} />
-                        
-                    </span>
-
-                    <button>See more</button>
-</section>
-                </div>
-            )}
-</div>
+                {data.allContentfulProject.nodes.map(project =>
+                    <div className={style.projectwrapper} key={project.id} >
+                        <div className={style.textcontainer}>
+                            <h2>{project.title}</h2></div>
+                        <div className={style.imageholder}>
+                            <img src={project.image[0].url} alt="" className={style.projectimg} />
+                        </div>
+                        <span>
+                            {project.hashtags.map(tag =>
+                                <p>{tag}</p>
+                            )}
+                        </span>
+                        <section className={style.buttonscontainer}>
+                            <span className={style.linkscontainer}>
+                                {project.githubLink && (
+                                    <a href={project.githubLink}>
+                                        <img src={GitIcon} alt="GitHub" className={style.projecticons} />
+                                    </a>
+                                )}
+                                {project.pageLink && (
+                                    <a href={project.pageLink}>
+                                        <img src={Openexternal} alt="OpenExternal" className={style.projecticons} /></a>
+                                )}
+                            </span>
+                                    
+                            <button><Link to={'/project/' + project.title}>
+                                See more</Link></button>
+                        </section>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
